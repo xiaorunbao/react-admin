eda9e0d7684ae52580e172170852e856f859e9dc
xiaorunbao <542929781@qq.com>
2021-03-26 12:41:36 +0800

    feat: add user

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -10,7 +10,7 @@ import Home from "../home/home";
 import Category from "../category/category";
 import Product from "../product/product";
 import Role from "../role/role";
-// import User from "../user/user";
+import User from "../user/user";
 // import Bar from "../charts/bar";
 // import Line from "../charts/line";
 // import Pie from "../charts/pie";
@@ -43,8 +43,8 @@ export default class Admin extends Component {
               <Route path="/category" component={Category} />
               <Route path="/product" component={Product} />
               <Route path="/role" component={Role} />
-              {/* <Route path="/user" component={User} />
-              <Route path="/charts/bar" component={Bar} />
+              <Route path="/user" component={User} />
+              {/*    <Route path="/charts/bar" component={Bar} />
               <Route path="/charts/pie" component={Pie} />
               <Route path="/charts/line" component={Line} />
               <Route path="/order" component={Order} />*/}
